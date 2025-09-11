21776f999aabce88dea6f61b2a94d9f70a19a38b
Arco Mahmud <arco.smahmud@gmail.com>
2025-09-11 16:29:30 +0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi(); // Minimal API OpenAPI support
+
+// Enable CORS for Angular frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular",
+        policy => policy.WithOrigins("http://localhost:4200")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+// OpenAPI (JSON spec) in Development
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.UseCors("AllowAngular");
+app.MapControllers();
+
+app.Run();
