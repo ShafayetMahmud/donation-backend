6868c19172d2c89e990c0aacebf542cb7f48f64e
ShafayetMahmudArco <arco.smahmud@gmail.com>
2025-10-12 01:56:54 +0600

    build update

--- obj/Debug/net9.0/BkashBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/BkashBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BkashBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+748336c3eb6a5cd7f62624180326598de8f072f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+057bd7a74ca88ef3c35a13d3f4e5762506ba77ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("BkashBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BkashBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
